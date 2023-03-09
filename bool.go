5ddbc923dd16e1312b8ff02fec67652545e17b72
Kacper Bąk <kacperbak290@gmail.com>
2023-03-09 20:25:54 +0100

    :sparkles: bool, string, README.md

--- bool.go
+++ bool.go
@@ -1,1 +1,6 @@
 package ptr
+
+// Bool returns a pointer to the bool value passed in.
+func Bool(b bool) *bool {
+	return &b
+}
--- string.go
+++ string.go
@@ -1,1 +1,6 @@
 package ptr
+
+// String returns a pointer to the string value passed in.
+func String(s string) *string {
+	return &s
+}
